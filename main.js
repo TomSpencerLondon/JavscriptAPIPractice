c72b889dd070243b17a10a4e78b3a17a48286239
Tom <thomas.spencer@codurance.com>
2021-02-24 14:30:27 +0000

    Initial commit

---
+++ main.js
@@ -0,0 +1,38 @@
+function getAlbums() {
+
+  const rightColumn = document.getElementsByClassName("rightColumn")[0];
+
+  const debugElement = document.getElementById("debug");
+
+  const fetchPromise = fetch(
+    "https://itunes.apple.com/us/rss/topalbums/limit=5/json"
+  );
+
+  fetchPromise
+    .then((d) => d.json())
+    .then((data) => {
+      const albumEntries = data.feed.entry;
+
+      // TODO1: understand the mapping dunction, and add price attribute to the album
+      const albumsRemapped = albumEntries.map((album) => {
+        return {
+          name: album.title.label,
+          price: album["im:price"].label,
+        }
+      });
+
+      // TODO2: think about (but neccesairly implement), on how we could display albums as bulletpoints on the webpage
+
+      return (rightColumn.innerText = JSON.stringify(
+        albumsRemapped,
+        null,
+        4
+      ));
+    })
+    .catch((error) => {
+      debugElement.innerText = "error: " + error.toString();
+    });
+}
+
+
+document.addEventListener('DOMContentLoaded', getAlbums);
